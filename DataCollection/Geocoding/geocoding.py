488e54ad8ffd06af8d10a678e578f2c40b5604d9
itsalys <69067945+itsalys@users.noreply.github.com>
2025-03-03 04:09:19 +0800

    data cleaning + calculating no. of locations and distance to nearest.

---
+++ DataCollection/Geocoding/geocoding.py
@@ -0,0 +1,24 @@
+import googlemaps
+import pandas as pd
+
+
+# Google Maps API Key (Replace with your own)
+API_KEY = "AIzaSyAFcW2PI7m1gWUIU8h8cGckGFKIL1QsLGw"
+
+# Initialize Google Maps Client
+gmaps = googlemaps.Client(key=API_KEY)
+
+# Function to get longitude and latitude from address
+def get_coordinates(address):
+    print(f"Geocoding - {address}")
+    try:
+        result = gmaps.geocode(address)
+        if result:
+            location = result[0]['geometry']['location']
+            return location['lng'], location['lat']  # Return as (Longitude, Latitude)
+    except Exception as e:
+        print(f"Error fetching {address}: {e}")
+    return None, None  # Return None if not found
+
+if __name__ == "__main__":
+    print(get_coordinates("252 Houngang Ave 3"))
